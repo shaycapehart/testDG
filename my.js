1701c51dd5eded1a925dc4111794e3b1f346f1e7
shaycapehart <shay.capehart@gmail.com>
2020-06-25 03:30:21 -0700

    Update my.js

--- my.js
+++ my.js
@@ -9,6 +9,23 @@ function testGS(){
     })
 }
 
-document.getElementById("btn").addEventListener("click", testGS)
+function addGS(){
+  var url = "https://script.google.com/macros/s/AKfycby5e74utExDwDzLlyGa5SO5WG3jqDsj3wI4c70SFiUNDpZC4ek/exec"
+
+  fetch(url, {
+    method: "POST",
+    mode: "no-cors",
+    cache: "no-cache",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow',
+    body: JSON.stringify({name: "Olivia"})
+
+  })
+}
+
+document.getElementById("read-btn").addEventListener("click", testGS)
+document.getElementById("create-btn").addEventListener("click", addGS)
 
 
